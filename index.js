b0aa4ba38ca7c222574ef5947de97eede1098ed1
Bilel <b.oueslati@ewm.swiss>
2023-03-07 11:47:33 +0100

    test api

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const compression = require("compression");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 
-const dev = process.env.NODE_ENV !== "production";
-
 const app = next({ dev: false });
 
 const handle = app.getRequestHandler();
@@ -16,10 +14,6 @@ app.prepare().then(() => {
   const server = express();
 
   server.use(compression());
-
-  server.get("/_next/*", (req, res) => {
-    handle(req, res);
-  });
 
   server.all("*", (req, res) => {
     handle(req, res);
