3c71897a4703f03cb8af44f00b94ad6f9844c1e4
Bilel <b.oueslati@ewm.swiss>
2023-03-07 12:20:59 +0100

    Update index server

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const next = require("next");
 
 const compression = require("compression");
 
+const { parse } = require("url");
+
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 const app = next({ dev: false });
 
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+const server = express();
 
+app.prepare().then(() => {
   server.use(compression());
 
   server.all("*", (req, res) => {
-    handle(req, res);
+    const { url } = req;
+
+    const parsedUrl = parse(url, true);
+
+    handle(req, res, parsedUrl);
   });
 
   server.listen(port, (err) => {
