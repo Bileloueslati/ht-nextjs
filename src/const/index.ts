a8bd328f700f1b0317ffc721839a36c997bf3429
Bilel <b.oueslati@ewm.swiss>
2023-03-09 11:39:01 +0100

    minor update

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,7 +1,7 @@
-import http from "@/libs/axios";
+import { Api } from "@/libs/axios";
 
 export const API_ENDPOINT = "https://www.ht-api.canfianceesthetique.com";
 
 export const CRM_URL = "https://perso.healthtravel.fr";
 
-export const fetchFromApi = <T= any>(url: string) => http.get<T>(url);
+export const fetchFromApi = <T = any>(url: string) => Api.get<T>(url);
